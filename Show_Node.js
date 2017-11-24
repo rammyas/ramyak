38904700c4e558cf5ffbc8e4edfd62033ea0502e
Ramya <ramya.karthikeyan@accenture.com>
2017-11-24 17:24:14 +0530

    R2 changes

--- Show_Node.js
+++ Show_Node.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var dt = require('./myFirstModule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
+    res.write("The date and time are currently: " + dt.myDateTime());
+	res.write(req.url);
     res.end('WELCOME');
 }).listen(8080);
